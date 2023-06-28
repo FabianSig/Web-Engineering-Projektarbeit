095cc1390b5851105825ebbafae40fac9f252aa3
fabiansig <fabian.sigmund1@gmail.com>
2023-06-28 17:02:18 +0200

    user-profile

--- Web-Engineering-Projektarbeit/src/app/user-profile/user-profile.component.ts
+++ Web-Engineering-Projektarbeit/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { Userdata } from '../userdata';
+import { UserserviceService } from '../userservice.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,7 +9,30 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent {
+
   @Input() 
   username? :string;
 
+  userdata? :Userdata;
+  userdata$: Observable<Userdata>;
+
+  constructor(private userservice: UserserviceService) {
+    this.userdata$ = userservice.getUser(this.username?)
+  }
+
+
+  //profilbild
+  //beigetreten
+  //Commit anzahl
+  // öffentliche Repos
+  // follower
+  // folgt
+  // achievments
+  // letzter commit
+  
+
 }
+function getUser(username: string | undefined): Userdata {
+  throw new Error('Function not implemented.');
+}
+
