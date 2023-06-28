2340a5f29a90b3e2c748f1257e527e7402a8ca8f
Niko <nikobo2003@gmail.com>
2023-06-28 15:38:05 +0200

    go button function

--- Web-Engineering-Projektarbeit/src/app/app.component.ts
+++ Web-Engineering-Projektarbeit/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Web-Engineering-Projektarbeit';
+
+  constructor() { }
+
+  goCompare(){
+    console.log("goCompare");	
+  }
 }
