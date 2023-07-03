c088cd0cebbe52d17303f1b815383f7e1410ebf5
Niko <nikobo2003@gmail.com>
2023-07-03 15:23:14 +0200

    get repositories

--- Web-Engineering-Projektarbeit/src/app/userservice.service.ts
+++ Web-Engineering-Projektarbeit/src/app/userservice.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, catchError, throwError } from 'rxjs';
 import { Userdata } from './userdata';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Repository } from './shared/repository';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,10 @@ export class UserserviceService {
     return this.http.get<Userdata>(`${this.apiUrl}/users/${username}`).pipe(catchError(this.handleError));
   }
 
+  /*getContributions(username: string): Observable<any> {
+    return this.http.get(`https://github.com/users/${username}/contributions`);
+  }*/
+
   handleError(error: HttpErrorResponse) {
     if (error.status === 0) console.error("client error lol");
     else if (error.status === 404) { 
@@ -28,4 +33,37 @@ export class UserserviceService {
     }
     return throwError(() => new Error('Something bad happened; please try again later.'));
   }
+
+  getRepositories(username: string): Observable<Array<Repository> {
+    return this.http.get<Array<Repository>>(`${this.apiUrl}/users/${username}/repos`);
+  }
+
+  /*async getContributions(username: string) {
+    const body = {
+        "query": `query {
+            user(login: "${username}") {
+              name
+              contributionsCollection {
+                contributionCalendar {
+                  colors
+                  totalContributions
+                  weeks {
+                    contributionDays {
+                      color
+                      contributionCount
+                      date
+                      weekday
+                    }
+                    firstDay
+                  }
+                }
+              }
+            }
+          }`
+    }
+    const response = await fetch('https://api.github.com/graphql', { method: 'POST', body: JSON.stringify(body)})
+    const data = await response.json()
+    console.log(data)
+    return data
+}*/
 }
