73cf07f9a77cf95bb9bcb69ceb41ff94dced5c95
fabiansig <fabian.sigmund1@gmail.com>
2023-07-04 18:19:29 +0200

    footer & made with heart

--- Web-Engineering-Projektarbeit/src/app/comparison-container/comparison-container.component.ts
+++ Web-Engineering-Projektarbeit/src/app/comparison-container/comparison-container.component.ts
@@ -41,80 +41,90 @@ export class ComparisonContainerComponent implements OnInit{
     this.userOneWinBoolArr = [false, false, false, false, false, false];
   }
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.usernameOne = params["nameone"];
-      this.usernameTwo = params["nametwo"];
-    });
 
-    this.userTwoWinBoolArr = [false, false, false, false, false, false];
-    this.userOneWinBoolArr = [false, false, false, false, false, false];
-
-    this.route.paramMap.pipe(
-      map(params => params.get('nameone')!),
-      switchMap(nameone => this.userservice.getUser(nameone))
-    ).subscribe(userdata => {
-
-      this.userdataOne = userdata
-      this.route.paramMap.pipe(
-        map(params => params.get('nametwo')!),
-        switchMap(nametwo => this.userservice.getUser(nametwo))
-      ).subscribe(userdata =>{
-        
-        this.userdataTwo = userdata
-        this.userOneWinBoolArr[1] = this.userdataOne!.followers! > this.userdataTwo.followers;
-        this.userTwoWinBoolArr[1] = this.userdataOne!.followers! < this.userdataTwo.followers;
-
-        this.userOneWinBoolArr[3] = this.userdataOne!.public_repos! > this.userdataTwo.public_repos;
-        this.userTwoWinBoolArr[3] = this.userdataOne!.public_repos! < this.userdataTwo.public_repos;
-
-        this.userOneWinBoolArr[4] = Date.parse(this.userdataOne!.created_at) > Date.parse(this.userdataTwo!.created_at);
-        this.userOneWinBoolArr[4] = Date.parse(this.userdataOne!.created_at) < Date.parse(this.userdataTwo!.created_at);
-
-        this.route.paramMap.pipe(
-          map(params => params.get('nameone')!),
-          switchMap(nameone => this.userservice.getRepositories(nameone))
-        ).subscribe(repos =>{ 
-    
-          this.stargazerOneCount = this.countStargazers(repos)
-          this.route.paramMap.pipe(
-            map(params => params.get('nametwo')!),
-            switchMap(nametwo => this.userservice.getRepositories(nametwo))
-          ).subscribe(repos =>{ 
-    
-            this.stargazerTwoCount = this.countStargazers(repos)
-            this.userOneWinBoolArr[2] = this.stargazerOneCount > this.stargazerTwoCount;
-            this.userTwoWinBoolArr[2] = this.stargazerOneCount < this.stargazerTwoCount;
-
-            this.route.paramMap.pipe(
-              map(params => params.get('nameone')!),
-              switchMap(nameone => this.userservice.getContributions(nameone))
-            ).subscribe(contributions =>{
-        
-              this.userContributionsOneCount = this.countContributions(contributions)
-              this.route.paramMap.pipe(
-                map(params => params.get('nametwo')!),
-                switchMap(nametwo => this.userservice.getContributions(nametwo))
-              ).subscribe(contributions =>{
-        
-              this.userContributionsTwoCount = this.countContributions(contributions);
-              this.userOneWinBoolArr[0] = this.userContributionsOneCount > this.userContributionsTwoCount;
-              this.userTwoWinBoolArr[0] = this.userContributionsOneCount < this.userContributionsTwoCount;
-
-              this.count = 0;
-              this.userOneWinBoolArr.forEach(element => {if(element) this.count++;});
-
-              this.userOneWinBoolArr[5] = this.count > 2
-              this.userTwoWinBoolArr[5] = this.count <= 2;
-        
-            });
-
-          });
-          });
-        });
-    
-        
+    try{
+      this.route.params.subscribe(params => {
+        this.usernameOne = params["nameone"];
+        this.usernameTwo = params["nametwo"];
       });
-    });
+  
+      this.userTwoWinBoolArr = [false, false, false, false, false, false];
+      this.userOneWinBoolArr = [false, false, false, false, false, false];
+  
+      this.route.paramMap.pipe(
+        map(params => params.get('nameone')!),
+        switchMap(nameone => this.userservice.getUser(nameone))
+      ).subscribe(userdata => {
+  
+        this.userdataOne = userdata
+        this.route.paramMap.pipe(
+          map(params => params.get('nametwo')!),
+          switchMap(nametwo => this.userservice.getUser(nametwo))
+        ).subscribe(userdata =>{
+          
+          this.userdataTwo = userdata
+          this.userOneWinBoolArr[1] = this.userdataOne!.followers! > this.userdataTwo.followers;
+          this.userTwoWinBoolArr[1] = this.userdataOne!.followers! < this.userdataTwo.followers;
+  
+          this.userOneWinBoolArr[3] = this.userdataOne!.public_repos! > this.userdataTwo.public_repos;
+          this.userTwoWinBoolArr[3] = this.userdataOne!.public_repos! < this.userdataTwo.public_repos;
+  
+          this.userOneWinBoolArr[4] = Date.parse(this.userdataOne!.created_at) > Date.parse(this.userdataTwo!.created_at);
+          this.userOneWinBoolArr[4] = Date.parse(this.userdataOne!.created_at) < Date.parse(this.userdataTwo!.created_at);
+  
+          this.route.paramMap.pipe(
+            map(params => params.get('nameone')!),
+            switchMap(nameone => this.userservice.getRepositories(nameone))
+          ).subscribe(repos =>{ 
+      
+            this.stargazerOneCount = this.countStargazers(repos)
+            this.route.paramMap.pipe(
+              map(params => params.get('nametwo')!),
+              switchMap(nametwo => this.userservice.getRepositories(nametwo))
+            ).subscribe(repos =>{ 
+      
+              this.stargazerTwoCount = this.countStargazers(repos)
+              this.userOneWinBoolArr[2] = this.stargazerOneCount > this.stargazerTwoCount;
+              this.userTwoWinBoolArr[2] = this.stargazerOneCount < this.stargazerTwoCount;
+  
+              this.route.paramMap.pipe(
+                map(params => params.get('nameone')!),
+                switchMap(nameone => this.userservice.getContributions(nameone))
+              ).subscribe(contributions =>{
+          
+                this.userContributionsOneCount = this.countContributions(contributions)
+                this.route.paramMap.pipe(
+                  map(params => params.get('nametwo')!),
+                  switchMap(nametwo => this.userservice.getContributions(nametwo))
+                ).subscribe(contributions =>{
+          
+                this.userContributionsTwoCount = this.countContributions(contributions);
+                this.userOneWinBoolArr[0] = this.userContributionsOneCount > this.userContributionsTwoCount;
+                this.userTwoWinBoolArr[0] = this.userContributionsOneCount < this.userContributionsTwoCount;
+  
+                this.count = 0;
+                this.userOneWinBoolArr.forEach(element => {if(element) this.count++;});
+  
+                this.userOneWinBoolArr[5] = this.count > 2
+                this.userTwoWinBoolArr[5] = this.count <= 2;
+          
+              });
+  
+            });
+            });
+          });
+      
+          
+        });
+      });
+    }
+
+    catch(e: any){
+
+      console.log("Das ist die Nachricht: " + e.name)
+      console.log("Das ist die Nachricht: " + e.message)
+    }
+    
   }
 
   countStargazers(repos: Array<Repository>): number{
